de7bdb8f6a2b713ae0fa4af7ec295599a6f80596
Xin <freedom_365@msn.com>
2016-10-09 00:06:29 +0800

    try to remove jsessionid in redirection url but failed by disordered execution, use tomcat instead

--- server/src/main/java/wang/xin/robocode/server/configs/AppConfig.java
+++ server/src/main/java/wang/xin/robocode/server/configs/AppConfig.java
@@ -3,17 +3,25 @@ package wang.xin.robocode.server.configs;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
 
 /**
  * Created by Xin on 2016/10/6.
  */
 @Configuration
-public class AppConfig {
+public class AppConfig implements ServletContextInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        //servletContext.setInitParameter("org.eclipse.jetty.servlet.SessionIdPathParameterName", "none");
+    }
 
     @Bean
     public Jackson2ObjectMapperBuilder jackson2ObjectMapperBuilder() {
